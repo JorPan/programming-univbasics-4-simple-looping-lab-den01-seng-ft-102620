4d203156b3c2a9f832fd27fa2969cea23c1495f6
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-09-25 18:28:01 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -6,6 +6,7 @@ def loop_message_five_times(string)
   end
 end
 
+#this should be working??? so confused... 
 
 def loop_message_n_times(string, n)
   i = 0 
@@ -15,4 +16,13 @@ def loop_message_n_times(string, n)
   end
 end
 
+
+def output_array(array)
+  i = 0
+  seperate = []
+  while i < array.length do
+    seperate << array[i]
+    i += 1 
+  end
+end
 
