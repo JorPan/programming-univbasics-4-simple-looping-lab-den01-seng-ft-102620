7bbb94909abd55a6f4475c4506ab4e199e2642b3
Jordan Panasewicz <jbpanasewicz@gmail.com>
2020-09-25 18:17:34 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,17 +1,19 @@
 def looping_message_five_times(string)
-  i = 0 
+  i = 0
+  message = []
   while i < 5 
-  puts string
+  message << string
   i += 1 
   end
+  return message
 end
 
 
 def loop_message_n_times(string, n)
   i = 0
-  while i < n 
-  puts string 
-  i += 1 
-end
-  
+  message = []
+    while i < n 
+    puts string 
+    i += 1 
+    end
 end
